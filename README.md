6a5847b3193543a0753a6eafbafde53a730c91c7
Paweł Kowalski <kowalski.pawel.r@gmail.com>
2024-01-23 11:59:50 +0100

    add entire project

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# pathfinding-algorithms
-This repository contains an implementation of pathfinding algorithms in C++
+# Pathfinding Algorithms in C++
+
+This repository contains an implementation of the A* search algorithm in C++. The goal of this code is to find an optimal (or near-optimal) path in terms of distance between two points on a map imported from a `.pgm` file.
+
+## Getting Started
+
+To use this project, follow these steps to build it:
+
+```bash
+mkdir build
+
+# If you don't need to build the tests, you can omit the BUILD_TESTING flag.
+cmake -S . -B ./build -DBUILD_TESTING=ON 
+
+make -C ./build
+```
+
+After building the project, you can run the binary by navigating to the build directory and executing the binary file. Here's how you can do it:
+
+```bash
+cd ./build
+./task_1 "../maps/willow.pgm" 155 37 285 585
+```
+
+Alternatively, you can use a different map and start/end points:
+
+```bash
+ ./task_1 "../maps/factory.pgm" 500 100 2200 340
+```
+
+If you have previously built the tests, you can run them by navigating to the build directory and executing the `ctest` command:
+
+```bash
+cd ./build
+ctest
+```
+
+## Code Structure & Features
+
+The architecture outlined above supports the use of pre-existing pathfinding A* algorithm, as well as the development of custom solutions using the same interface.
+
+![code_structure](./docs/code_structure.png)
+
+## Results
+
+This A* implementation allows a node, once reached and removed from the `frontier`, to be added back if a less costly path is found. This is crucial to ensure the returned path is the best possible when the heuristic function is admissible but not consistent. If the heuristic is consistent, the path to a node removed from the `frontier` is guaranteed to be the most efficient. The main goal of this task is to implement an efficient pathfinding algorithm. However, to decrease execution time, an enhancement is available that provides a solution that is generally satisfactory and at worst, slightly less than the best. When enabled, this enhancement avoids revisiting previously checked nodes, significantly reducing computation time. However, it does not guarantee the best solution as the algorithm will stop exploring other paths once it finds a path to the goal.
+
+|                          Method | Execution Time (ms) | Nodes Inspected | Result File                                                                                       |
+| ------------------------------: | :-----------------: | :-------------: | ------------------------------------------------------------------------------------------------- |
+|         Euclidean Best Solution |         373         |     184669      | [factory_euclidean_best_solution.pgm](./docs/factory_euclidean_best_solution.pgm)                 |
+|         Manhattan Best Solution |         123         |      68552      | [factory_manhattan_best_solution.pgm](./docs/factory_manhattan_best_solution.pgm)                 |
+| Manhattan High-Quality Solution |         56          |      14702      | [factory_manhattan_high_quality_solution.pgm](./docs/factory_manhattan_high_quality_solution.pgm) |
+| Euclidean High-Quality Solution |         333         |      47159      | [factory_euclidean_high_quality_solution.pgm](./docs/factory_euclidean_high_quality_solution.pgm) |
+
+There are several strategies available to enhance the efficiency and computational performance of the A* algorithm. This implementation provides the flexibility to adjust the cost of movement between two neighboring cells. As the cost of movement between cells decreases, the heuristic's influence on the decision-making process intensifies. This directly enhances the overall performance of the pathfinding process, as fewer cells are evaluated. Another approach is to redefine a neighbour node as a cell that is, for example, 3 pixels away (define chunks of pixels), rather than a directly adjacent cell. This reduces the number of nodes the algorithm needs to inspect, potentially significantly improving execution time. Another strategy is to use the squared Euclidean distance `dx^2 + dy^2` instead of the standard Euclidean distance formula `sqrt(dx^2 + dy^2)`. This eliminates the need for a computationally expensive square root operation, thereby reducing the algorithm's computational demand.
+
+Please note that while these improvements may not always yield the best path, they can provide high-quality solutions much more quickly.
---
+++ include/pathfinding/utils.hpp
@@ -0,0 +1,303 @@
+#ifndef PATHFINDING_UTILS_HPP_
+#define PATHFINDING_UTILS_HPP_
+
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+namespace pathfinding_utils
+{
+
+struct Node
+{
+  /**
+   * @brief Coordinate of the node
+   */
+  int x, y;
+
+  /**
+   * @brief Cost to reach the node from the start node
+   */
+  float g;
+
+  /**
+   * @brief Estimated cost to reach the goal node from the current node
+   */
+  float h;
+
+  /**
+   * @brief Sum of g and h
+   */
+  float f;
+};
+
+struct NodeHash
+{
+  /**
+   * @brief Generates a hash value for a Node. This is used for the unordered_map container.
+   * @param node The Node object to hash.
+   * @return std::size_t The hash value generated.
+   */
+  std::size_t operator()(const Node & node) const
+  {
+    return std::hash<int>()(node.x) ^ std::hash<int>()(node.y);
+  }
+};
+
+bool operator==(const Node & lhs, const Node & rhs);
+
+bool operator!=(const Node & lhs, const Node & rhs);
+
+/**
+ * @brief Compares two nodes based on their f values.
+ *
+ * @param lhs The left-hand side node.
+ * @param rhs The right-hand side node.
+ *
+ * @return true if the f value of lhs is greater than that of rhs.
+ *
+ * @note The logic in the operator< function is negated because it is used for a priority queue in
+ * the A* algorithm. In the A* algorithm, the node with the lowest f value (the sum of the cost to
+ * reach the node and the estimated cost to the goal) is considered the highest priority. Therefore,
+ * the nodes are sorted in descending order of f values, which is achieved by using the greater-than
+ * operator (>) instead of the less-than operator (<).
+ */
+bool operator<(const Node & lhs, const Node & rhs);
+
+/**
+ * @brief Enum representing cell occupancy levels.
+ *
+ * @note The values of the enum are negated compared to the values of the pixels in the .pgm file
+ * due to the definitions of free and occupied thresholds.
+ */
+enum CellOccupancyLevel : uint8_t {
+  FREE = 0,
+  OCCUPIED = 255,
+};
+
+class OccupancyGrid
+{
+public:
+  OccupancyGrid() : width_(0), height_(0), max_value_(0) {}
+
+  /**
+   * @brief Constructor that loads an occupancy grid from a file.
+   *
+   * @param filename The filename of the PGM file.
+   * @param free_threshold The threshold for free cells.
+   */
+  OccupancyGrid(const std::string filename, const uint8_t free_threshold = 40);
+
+  /**
+   * @brief Constructor to initialize an occupancy grid with provided parameters.
+   *
+   * @param pixels The pixel data representing the grid.
+   * @param width The width of the grid.
+   * @param height The height of the grid.
+   * @param max_value The maximum pixel value.
+   */
+  OccupancyGrid(
+    std::vector<uint8_t> pixels, const unsigned width, const unsigned height,
+    const unsigned max_value);
+
+  /**
+   * @brief Saves the occupancy grid as a PGM file. The file extension is automatically appended
+   * if it is not provided.
+   *
+   * @throws std::runtime_error if unable to open the file or if map size does not match the
+   * number of pixel data
+   *
+   * @param filename The filename for the saved PGM file.
+   *
+   * @note The pixel values are inverted before saving. In .pgm format, 0 represents black and 255
+   * represents white, while in the OccupancyGrid class, 0 (associated with white color)
+   * represents free due to the definitions of the free and occupied thresholds.
+   */
+  void SaveAsPGM(const std::string filename) const;
+
+  /**
+   * @brief Checks if a specific cell is occupied.
+   *
+   * @param x The x-coordinate of the cell.
+   * @param y The y-coordinate of the cell.
+   *
+   * @throws std::out_of_range if the coordinates are out of range.
+   *
+   * @return bool True if the cell is occupied, false otherwise.
+   */
+  bool IsOccupied(int x, int y) const;
+
+  /**
+   * @brief Draws a path on the occupancy grid.
+   *
+   * @param path The path to draw.
+   * @param color The color of the path (0-255). Default value is 120.
+   */
+  void DrawPath(const std::vector<Node> & path, const uint8_t color = 120);
+
+  unsigned GetWidth() const { return width_; }
+  unsigned GetHeight() const { return height_; }
+  unsigned GetMaxValue() const { return max_value_; }
+
+  /**
+   * @brief Accesses the pixel value at the specified coordinates.
+   *
+   * @param x The x-coordinate of the pixel.
+   * @param y The y-coordinate of the pixel.
+   *
+   * @return uint8_t The pixel value.
+   *
+   * @throws std::out_of_range If the pixel coordinates are out of range.
+   */
+  uint8_t GetPixel(const int x, const int y) const
+  {
+    if (x < 0 || x >= width_ || y < 0 || y >= height_) {
+      throw std::out_of_range("Pixel coordinates out of range");
+    }
+    return pixels_[y * width_ + x];
+  }
+
+  /**
+   * @brief Sets the pixel value at the specified coordinates.
+   *
+   * @param x The x-coordinate of the pixel.
+   * @param y The y-coordinate of the pixel.
+   * @param value The new pixel value.
+   *
+   * @throws std::out_of_range If the pixel coordinates are out of range.
+   * @throws std::runtime_error If the new pixel value is greater than the maximum value.
+   */
+  void SetPixel(const int x, const int y, const uint8_t value)
+  {
+    if (x < 0 || x >= width_ || y < 0 || y >= height_) {
+      throw std::out_of_range("Pixel coordinates out of range");
+    }
+    if (value > max_value_) {
+      throw std::runtime_error("Pixel value is greater than max value");
+    }
+    pixels_[y * width_ + x] = value;
+  }
+
+private:
+  /**
+   * @brief Get the vector of pixels from .pgm file.
+   *
+   * @param filename The filename of the .pgm file.
+   *
+   * @throws std::runtime_error if unable to open the file or if the file is not in .pgm format
+   *
+   * @note The pixel values are inverted before saving. In .pgm format, 0 represents black and 255
+   * represents white, while in the OccupancyGrid class, 0 (associated with white color) represents
+   * free due to the definitions of the free and occupied thresholds.
+   */
+  void GetGridFromPGM(const std::string & filename, const uint8_t free_threshold);
+
+  /**
+   * @brief Validates the header of the .pgm file (max_value, width, height)
+   *
+   * @throws std::runtime_error if the header is invalid
+   */
+  void ValidateHeader() const;
+
+  /**
+   * @brief Validates the size of the .pgm file (width * height == number of pixel data)
+   *
+   * @throws std::runtime_error if the size is invalid
+   */
+  void ValidateSize() const;
+
+  /**
+   * @brief Thresholds the pixel value based on the free threshold. The pixels with values less
+   * than or equal to the free threshold are considered free. Other pixels are considered
+   * occupied.
+   *
+   * @param free_threshold The threshold for free cells.
+   */
+  static void ThresholdPixelValue(uint8_t & pixel, const uint8_t free_threshold);
+
+  /**
+   * @brief Invert the pixel value.
+   *
+   * In .pgm format, 0 represents black and 255 represents white, while in the OccupancyGrid class,
+   * 0 (associated with white color) represents free due to the definitions of the free and occupied
+   * thresholds.
+   *
+   * @param pixel The pixel value to invert.
+   */
+  static void InvertPixelValue(uint8_t & pixel);
+
+  /**
+   * @brief Ensures that the filename has the .pgm extension.
+   *
+   * @param filename The filename to check.
+   *
+   * @return std::string The filename with the .pgm extension.
+   */
+  static std::string EnsurePGMExtension(const std::string & filename);
+
+  unsigned width_;
+  unsigned height_;
+  unsigned max_value_;
+  std::vector<uint8_t> pixels_;
+};
+
+class Heuristic
+{
+public:
+  virtual ~Heuristic() = default;
+
+  /**
+   * @brief Calculates the heuristic value between two nodes.
+   *
+   * @param start The starting node.
+   * @param end The goal node.
+   *
+   * @return float The calculated heuristic value.
+   */
+  virtual float Calculate(const Node & start, const Node & end) = 0;
+};
+
+class ManhattanHeuristic : public Heuristic
+{
+public:
+  virtual ~ManhattanHeuristic() = default;
+
+  /**
+   * @brief Calculates the Manhattan distance heuristic value.
+   *
+   * @param start The starting node.
+   * @param end The goal node.
+   *
+   * @return float The calculated heuristic value.
+   */
+  float Calculate(const Node & start, const Node & end) override
+  {
+    return std::abs(end.x - start.x) + std::abs(end.y - start.y);
+  }
+};
+
+class EuclideanHeuristic : public Heuristic
+{
+public:
+  virtual ~EuclideanHeuristic() = default;
+
+  /**
+   * @brief Calculates the Euclidean distance heuristic value.
+   *
+   * @param start The starting node.
+   * @param end The goal node.
+   *
+   * @return float The calculated heuristic value.
+   */
+  float Calculate(const Node & start, const Node & end) override
+  {
+    return std::hypot(end.x - start.x, end.y - start.y);
+  }
+};
+
+}  // namespace pathfinding_utils
+
+#endif  // PATHFINDING_UTILS_HPP_
